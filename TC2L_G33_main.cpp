1b82ce17670a0111bb2ad255cbb0cc25e817d719
Xiu Wei <xiuwei1205@gmail.com>
2025-04-30 12:24:57 +0800

    Initialise repository.

---
+++ TC2L_G33_main.cpp
@@ -0,0 +1,24 @@
+// *********************************************************
+// Program: TC2L_G33_main.cpp
+// Course: CCP6124 OOPDS
+// Lecture Class: TC2L
+// Tutorial Class: TT7L
+// Trimester: 2510
+// Member 1: 242UC244GR | Lee Xiu Wei    | LEE.XIU.WEI@student.mmu.edu.my    | 017-9218088
+// Member 2: 243UC24672 | Lai Seng Kung  | lai.seng.kung1@student.mmu.edu.my | 012-9169228
+// Member 3: 242UC244S3 | Teng Ming Hein | TENG.MING.HEIN@student.mmu.edu.my | 016-7831558
+// *********************************************************
+// Task Distribution
+// Member_1:
+// Member_2:
+// Member_3:
+// Member_4:
+// *********************************************************
+
+#include < iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <queue>
+using namespace std;
