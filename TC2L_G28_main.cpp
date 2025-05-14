7a3a249247aa5406ab2cad1fa039014235d96d96
Teng Ming Hein <tengminghein@gmail.com>
2025-05-14 14:54:41 +0800

    change file name

--- TC2L_G28_main.cpp
+++ TC2L_G28_main.cpp
@@ -1,11 +1,11 @@
 // *********************************************************
-// Program: TC2L_G33_main.cpp
+// Program: TC2L_G28_main.cpp
 // Course: CCP6124 OOPDS
 // Lecture Class: TC2L
 // Tutorial Class: TT7L
 // Trimester: 2510
-// Member 1: 242UC244GR | Lee Xiu Wei    | LEE.XIU.WEI@student.mmu.edu.my    | 017-9218088
-// Member 2: 243UC24672 | Lai Seng Kung  | lai.seng.kung1@student.mmu.edu.my | 012-9169228
+// Member 1: 243UC24672 | Lai Seng Kung  | lai.seng.kung1@student.mmu.edu.my | 012-9169228
+// Member 2: 242UC244GR | Lee Xiu Wei    | LEE.XIU.WEI@student.mmu.edu.my    | 017-9218088
 // Member 3: 242UC244S3 | Teng Ming Hein | TENG.MING.HEIN@student.mmu.edu.my | 016-7831558
 // *********************************************************
 // Task Distribution
